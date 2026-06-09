ca75a26679cd1d15d4549f8644dde89a23d7005c
eben-vranken <vranken.eben@outlook.com>
2026-06-09 15:40:17 +0200

    feat: add capped CounterVec with overflow collapse

--- promcap.go
+++ promcap.go
@@ -1,7 +1,57 @@
 package promcap
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const overflowValue = "__overflow__"
 
 type Cap struct {
 	reg prometheus.Registerer
+}
+
+func (c *Cap) NewCounterVec(opts prometheus.CounterOpts, labels []string, maxSeries int) *CappedCounterVec {
+	cv := prometheus.NewCounterVec(opts, labels)
+	c.reg.MustRegister(cv)
+
+	return &CappedCounterVec{
+		counterVec: cv,
+		maxSeries:  maxSeries,
+		seen:       make(map[string]struct{}),
+	}
+}
+
+func Wrap(reg prometheus.Registerer) *Cap {
+	return &Cap{reg: reg}
+}
+
+type CappedCounterVec struct {
+	counterVec *prometheus.CounterVec
+	maxSeries  int
+	seen       map[string]struct{}
+}
+
+func (ccv *CappedCounterVec) WithLabelValues(lvs ...string) prometheus.Counter {
+	key := strings.Join(lvs, "\xff")
+
+	_, ok := ccv.seen[key]
+
+	if ok {
+		return ccv.counterVec.WithLabelValues(lvs...)
+	}
+
+	if len(ccv.seen) < ccv.maxSeries {
+		ccv.seen[key] = struct{}{}
+		return ccv.counterVec.WithLabelValues(lvs...)
+	}
+
+	overflow := make([]string, len(lvs))
+
+	for i, _ := range overflow {
+		overflow[i] = overflowValue
+	}
+
+	return ccv.counterVec.WithLabelValues(overflow...)
 }
