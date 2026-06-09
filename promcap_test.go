bf74f7c980769a34452cf19014bf032e49e1ccad
eben-vranken <vranken.eben@outlook.com>
2026-06-09 15:58:43 +0200

    test: verify CounterVec caps series and collapses overflow

---
+++ promcap_test.go
@@ -0,0 +1,33 @@
+package promcap
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestSuccesfulPromcapInit(t *testing.T) {
+	reg := prometheus.NewRegistry()
+	regWrap := Wrap(reg)
+
+	cv := regWrap.NewCounterVec(prometheus.CounterOpts{Name: "request_total"}, []string{"user"}, 2)
+	cv.WithLabelValues("a").Inc()
+	cv.WithLabelValues("b").Inc()
+	cv.WithLabelValues("c").Inc()
+	cv.WithLabelValues("d").Inc()
+
+	count, err := testutil.GatherAndCount(reg)
+
+	if err != nil {
+		t.Fatalf("Inconsitent metrics")
+	}
+
+	if count != 3 {
+		t.Errorf("Gather and count got %d, want %d", count, 3)
+	}
+
+	if testutil.ToFloat64(cv.WithLabelValues(overflowValue)) != 2 {
+		t.Errorf("Overflow values got %v, want %v", testutil.ToFloat64(cv.WithLabelValues(overflowValue)), 2)
+	}
+}
