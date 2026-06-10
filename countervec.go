d5b6b2a7ff2d45561a574bab9958677dc9ded198
eben-vranken <vranken.eben@outlook.com>
2026-06-10 19:13:18 +0200

    refactor: register CappedCounterVec as a Collector wrapping the inner vec

--- countervec.go
+++ countervec.go
@@ -7,14 +7,18 @@ type CappedCounterVec struct {
 	lim        *limiter
 }
 
+var _ prometheus.Collector = (*CappedCounterVec)(nil)
+
 func (c *Cap) NewCounterVec(opts prometheus.CounterOpts, labels []string, capOpts CapOpts) *CappedCounterVec {
 	cv := prometheus.NewCounterVec(opts, labels)
-	c.reg.MustRegister(cv)
-
-	return &CappedCounterVec{
+	ccv := &CappedCounterVec{
 		counterVec: cv,
 		lim:        newLimiter(opts.Name, labels, capOpts, c.cappedTotal),
 	}
+
+	c.reg.MustRegister(ccv)
+
+	return ccv
 }
 
 func (ccv *CappedCounterVec) WithLabelValues(lvs ...string) prometheus.Counter {
@@ -24,3 +28,6 @@ func (ccv *CappedCounterVec) WithLabelValues(lvs ...string) prometheus.Counter {
 func (ccv *CappedCounterVec) With(labels prometheus.Labels) prometheus.Counter {
 	return ccv.counterVec.WithLabelValues(ccv.lim.resolve(ccv.lim.order(labels))...)
 }
+
+func (ccv *CappedCounterVec) Describe(ch chan<- *prometheus.Desc) { ccv.counterVec.Describe(ch) }
+func (ccv *CappedCounterVec) Collect(ch chan<- prometheus.Metric) { ccv.counterVec.Collect(ch) }
